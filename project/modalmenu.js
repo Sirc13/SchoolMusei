7a92d6e69c9ba648e3a9eb54e8add39cdd4dc812
kuryl <kurylevsasha200@gmail.com>
2025-10-20 17:27:53 +0300

    update(SchoolMusei): Обновление 1.5

--- project/modalmenu.js
+++ project/modalmenu.js
@@ -1,17 +1,26 @@
 // Функция для открытия модального окна
-function openModal(imageSrc, description) {
+function openModal(imageSrc, title) {
     const modal = document.getElementById('imageModal');
     const modalImage = document.getElementById('modalImage');
+    const modalTitle = document.getElementById('modalTitle');
     const modalDescription = document.getElementById('modalDescription');
     
     modalImage.src = imageSrc;
-    modalDescription.textContent = description;
+    modalTitle.textContent = title;
+    
+
+    
+    // Предотвращаем прокрутку фоновой страницы
+    document.body.style.overflow = 'hidden';
     modal.style.display = 'block';
 }
 
 // Функция для закрытия модального окна
 function closeModal() {
     const modal = document.getElementById('imageModal');
+    
+    // Восстанавливаем прокрутку фоновой страницы
+    document.body.style.overflow = 'auto';
     modal.style.display = 'none';
 }
 
@@ -29,3 +38,14 @@ document.addEventListener('keydown', function(event) {
         closeModal();
     }
 });
+
+// Предотвращаем прокрутку фона при прокрутке модального окна
+document.addEventListener('wheel', function(event) {
+    const modal = document.getElementById('imageModal');
+    if (modal.style.display === 'block') {
+        // Если модальное окно открыто, предотвращаем прокрутку фона
+        if (!event.target.closest('.modal-text') && !event.target.closest('.modal-content')) {
+            event.preventDefault();
+        }
+    }
+}, { passive: false });
