d70210d4ddcd41c7458af48e39b7de24cd392dbc
kuryl <kurylevsasha200@gmail.com>
2025-12-07 20:40:58 +0300

    update(SchoolMusei):  Обновление 2.5

--- project/SpecialPicture.js
+++ project/SpecialPicture.js
@@ -18,6 +18,21 @@ function getSpecialModalGallery(heroName) {
             </div>
         `;
     }
+    if (heroName === 'Сямтомов') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A1%D1%8F%D0%BC%D1%82%D0%BE%D0%BC%D0%BE%D0%B2%208%D0%B3/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A1%D1%8F%D0%BC%D1%82%D0%BE%D0%BC%D0%BE%D0%B2%208%D0%B3/2.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A1%D1%8F%D0%BC%D1%82%D0%BE%D0%BC%D0%BE%D0%B2%208%D0%B3/3.png" alt="" style="cursor: default;">
+                </div>
+            </div>
+        `;
+    }
     if (heroName === 'Горбунов Юрий Николаевич') {
         galleryHTML = `
             <div class="photo-gallery">
@@ -26,6 +41,160 @@ function getSpecialModalGallery(heroName) {
                 </div>
                 <div class="photo-item">
                     <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%91%D0%B0%D1%82%D0%B0%D1%80%D0%B5%D0%B5%D0%B2%D0%B0%208%D0%B2/2.png" alt="Фото 2 Горбунова" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Крюков') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9F%D1%80%D0%BE%D1%82%D0%BE%D0%BF%D0%BE%D0%BF%D0%BE%D0%B2%D0%B0%209%D0%B0/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9F%D1%80%D0%BE%D1%82%D0%BE%D0%BF%D0%BE%D0%BF%D0%BE%D0%B2%D0%B0%209%D0%B0/2.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Новосельцев') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B5%D0%BB%D1%8C%D1%86%D0%B5%D0%B2/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%9D%D0%BE%D0%B2%D0%BE%D1%81%D0%B5%D0%BB%D1%8C%D1%86%D0%B5%D0%B2/2.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Капустин') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9B%D1%8B%D1%87%D0%B0%D0%BA%D0%BE%D0%B2%D0%B0%208%D0%B2/4.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9B%D1%8B%D1%87%D0%B0%D0%BA%D0%BE%D0%B2%D0%B0%208%D0%B2/2.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Чистяков') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9B%D1%8B%D1%87%D0%B0%D0%BA%D0%BE%D0%B2%D0%B0%208%D0%B2/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9B%D1%8B%D1%87%D0%B0%D0%BA%D0%BE%D0%B2%D0%B0%208%D0%B2/3.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Грязов') {
+        galleryHTML = `
+            <div class="photo-gallery">
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%BE%D1%81%D0%B0%D1%80%D0%B5%D0%B2/1.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%BE%D1%81%D0%B0%D1%80%D0%B5%D0%B2/2.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%BE%D1%81%D0%B0%D1%80%D0%B5%D0%B2/3.png" alt="" style="cursor: default;">
+                </div>
+            </div>
+        `;
+    }
+    if (heroName === 'Соколов') {
+        galleryHTML = `
+            <div class="photo-gallery">
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%A1%D0%BE%D0%BA%D0%BE%D0%BB%D0%BE%D0%B2/1.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%A1%D0%BE%D0%BA%D0%BE%D0%BB%D0%BE%D0%B2/2.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%A1%D0%BE%D0%BA%D0%BE%D0%BB%D0%BE%D0%B2/3.png" alt="" style="cursor: default;">
+                </div>
+            </div>
+        `;
+    }
+    if (heroName === 'Гошев') {
+        galleryHTML = `
+            <div class="photo-gallery">
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%93%D0%BE%D1%88%D0%B5%D0%B2/1.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%93%D0%BE%D1%88%D0%B5%D0%B2/2.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%93%D0%BE%D1%88%D0%B5%D0%B2/3.png" alt="" style="cursor: default;">
+                </div>
+            </div>
+        `;
+    }
+    if (heroName === 'Кожанов') {
+        galleryHTML = `
+            <div class="photo-gallery">
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%9A%D0%BE%D0%B6%D0%B0%D0%BD%D0%BE%D0%B2/1.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%9A%D0%BE%D0%B6%D0%B0%D0%BD%D0%BE%D0%B2/2.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%9A%D0%BE%D0%B6%D0%B0%D0%BD%D0%BE%D0%B2/3.png" alt="" style="cursor: default;">
+               </div>
+               <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%9A%D0%BE%D0%B6%D0%B0%D0%BD%D0%BE%D0%B2/4.png" alt="" style="cursor: default;">
+               </div>
+            </div>
+        `;
+    }
+    if (heroName === 'Петухов') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9E%D0%BB%D0%B5%D0%B9%D0%BD%D0%B8%D0%BA/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9E%D0%BB%D0%B5%D0%B9%D0%BD%D0%B8%D0%BA/2.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Власов') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%92%D0%BB%D0%B0%D1%81%D0%BE%D0%B2/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%92%D0%BB%D0%B0%D1%81%D0%BE%D0%B2/2.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Гнедин') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%93%D0%BD%D0%B5%D0%B4%D0%B8%D0%BD/2.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%9D%D0%B5%D0%B8%D0%B7%D0%B2%D0%B5%D1%81%D1%82%D0%BD%D1%8B%D0%B5/%D0%93%D0%BD%D0%B5%D0%B4%D0%B8%D0%BD/1.png" alt="" style="cursor: default;">
                 </div>
                
             </div>
@@ -66,6 +235,50 @@ function getSpecialModalGallery(heroName) {
                     <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%91%D0%B0%D1%83%D0%BC%D0%B3%D0%B5%D1%80%D1%82%D0%BD%D0%B5%D1%80%208%D0%B2/2.png" alt="" style="cursor: default;">
                 </div>
                
+            </div>
+        `;
+    }
+    if (heroName === 'Шихов') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A8%D0%B8%D1%85%D0%BE%D0%B2%D0%B0%209%D0%B0/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A8%D0%B8%D1%85%D0%BE%D0%B2%D0%B0%209%D0%B0/2.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Хомутинников А. Н.') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A2%D1%83%D1%80%D0%BA%D0%B8%D0%BD%D0%B0%208%D0%B1/6.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A2%D1%83%D1%80%D0%BA%D0%B8%D0%BD%D0%B0%208%D0%B1/7.png" alt="" style="cursor: default;">
+                </div>
+               
+            </div>
+        `;
+    }
+    if (heroName === 'Хомутинников В. А.') {
+        galleryHTML = `
+            <div class="photo-gallery">
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A2%D1%83%D1%80%D0%BA%D0%B8%D0%BD%D0%B0%208%D0%B1/1.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A2%D1%83%D1%80%D0%BA%D0%B8%D0%BD%D0%B0%208%D0%B1/2.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A2%D1%83%D1%80%D0%BA%D0%B8%D0%BD%D0%B0%208%D0%B1/3.png" alt="" style="cursor: default;">
+                </div>
+                <div class="photo-item">
+                    <img src="https://4f267ac3-df8d-4c1e-a7b5-da5d95a9ecd1.selstorage.ru/%D0%92%D0%9E%D0%92/1%20%D1%87%D0%B0%D1%81%D1%82%D1%8C/%D0%A2%D1%83%D1%80%D0%BA%D0%B8%D0%BD%D0%B0%208%D0%B1/4.png" alt="" style="cursor: default;">
+                </div>
             </div>
         `;
     }
